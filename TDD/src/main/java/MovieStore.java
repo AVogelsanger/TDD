135b412b82cd2fd2111f49aaaae783ac2d204f17
AVogelsanger <voguednj@hotmail.com>
2020-06-19 00:16:10 -0300

    first commit

---
+++ TDD/src/main/java/MovieStore.java
@@ -0,0 +1,54 @@
+import model.Movie;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MovieStore {
+    List<Movie> movies = new LinkedList<Movie>();
+
+    public List<Movie> findByPartialTitle(final String partialTitle) {
+        Predicate predicate = movie -> movie.title().toUpperCase().contains(partialTitle.toUpperCase());
+           return findBy(predicate);
+    }
+
+    public void add(Movie movie) {
+        movies.add(movie);
+    }
+
+    public List<Movie> findByDirector(final String director) {
+        Predicate predicate = new Predicate() {
+            public boolean matches(Movie movie) {
+                return movie.director().equals(director);
+            }
+        };
+        List<Movie> result = new LinkedList<Movie>();
+        return findBy(predicate);
+
+    }
+
+    private List<Movie> findBy(Predicate predicate) {
+        List<Movie> result = new LinkedList<Movie>();
+        for (Movie movie : movies) {
+            if (predicate.matches(movie)) {
+                result.add(movie);
+            }
+        }
+        return result;
+    }
+
+    public List<Movie> findByReleaseYear(final int from, final int to) {
+        Predicate predicate = new Predicate() {
+            public boolean matches(Movie movie) {
+                return movie.releaseYear() > from && movie.releaseYear() < to;
+            }
+        };
+        List<Movie> result = new LinkedList<Movie>();
+        return findBy(predicate);
+
+    }
+
+
+    interface Predicate{
+        boolean matches(Movie movie);
+    }
+}
